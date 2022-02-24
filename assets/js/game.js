b960377fc0a031200e6d31bf1b24a0eda64b2dbc
Kaijam Jeffers <faceless128@gmail.com>
2022-02-24 17:51:57 -0500

    fixed blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,14 @@
 // Player parameters
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("what is your robot's name?");
+    }
+    console.log("Your robot's name is " + name)
+    return name;
+}
 var playerInfo = {
-    name: window.prompt("What is yout robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
